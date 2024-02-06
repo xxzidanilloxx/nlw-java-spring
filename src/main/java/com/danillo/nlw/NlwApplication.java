829cbbdba151e6328b0cf7a6c52b560789720051
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-02-06 19:27:06 -0300

    first commit

---
+++ src/main/java/com/danillo/nlw/NlwApplication.java
@@ -0,0 +1,13 @@
+package com.danillo.nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danillo/nlw/NlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danillo.nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
