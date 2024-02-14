156da6ea629fde81e68b0602e2465edcb4f2fd4d
xxzidanilloxx <wesleydanillo.silva@gmail.com>
2024-02-14 18:40:32 -0300

    inserção da anotação @Autowired para que seja realizada a injeção de dependência

--- src/main/java/com/danillo/nlw/modules/certifications/useCases/Top10RankingUseCase.java
+++ src/main/java/com/danillo/nlw/modules/certifications/useCases/Top10RankingUseCase.java
@@ -2,6 +2,7 @@ package com.danillo.nlw.modules.certifications.useCases;
 
 import com.danillo.nlw.modules.students.entities.CertificationStudent;
 import com.danillo.nlw.modules.students.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Service
 public class Top10RankingUseCase {
 
+    @Autowired
     private CertificationStudentRepository certificationStudentRepository;
 
     public List<CertificationStudent> execute(){
